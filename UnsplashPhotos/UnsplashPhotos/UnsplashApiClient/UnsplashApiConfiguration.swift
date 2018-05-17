baf2604d9293934d505db6d13b59d10e7cc84884
mirfat sharmin <mirfat.sharmin@gmail.com>
2018-05-17 10:53:09 -0400

    - Constructing url request by conforming to URLRequestConvertible

--- UnsplashPhotos/UnsplashPhotos/UnsplashApiClient/UnsplashApiConfiguration.swift
+++ UnsplashPhotos/UnsplashPhotos/UnsplashApiClient/UnsplashApiConfiguration.swift
@@ -9,13 +9,14 @@
 import Foundation
 import Alamofire
 
-enum UnsplashApiConfiguration {
+enum UnsplashApiConfiguration: URLRequestConvertible {
     
     case curatedPhotoList(page: Int, itemsPerPage: Int)
     
     private enum Constants {
         static let baseUrlPath = "https://api.unsplash.com"
         static let accessKey = "b9405cfb421a0aa04ce671a3c249842db5282998a2c8c8ac2eef24048ae17296"
+        static let timeoutInSeconds = 60
     }
     
     private enum ParameterKey {
@@ -25,6 +26,7 @@ enum UnsplashApiConfiguration {
         static let orderBy = "order_by"
     }
     
+    //MARK: URL Request Parameters
     private var path:String {
         switch self {
         case .curatedPhotoList:
@@ -44,5 +46,13 @@ enum UnsplashApiConfiguration {
         case .curatedPhotoList(let page, let itemsPerPage):
             return [ParameterKey.clientId: Constants.accessKey, ParameterKey.page: page, ParameterKey.itemsPerPage: itemsPerPage]
         }
+    }
+    
+    public func asURLRequest() throws -> URLRequest {
+        let url = try Constants.baseUrlPath.asURL()
+        var request = URLRequest(url: url.appendingPathComponent(path))
+        request.httpMethod = method.rawValue
+        request.timeoutInterval = TimeInterval(Constants.timeoutInSeconds)
+        return try URLEncoding.default.encode(request, with: parameters)
     }
 }
